ae88c9045251994fda88467eb3a8c864e1f635fb
hglong16 <intihad.vuong@gmail.com>
2023-06-05 17:07:31 +0700

    handleOnChangeFirstName done

---
+++ src/CustomerForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const CustomerForm = ({
+  original,
+  onSubmit,
+}) => {
+  const [customer, setCustomer] = useState(original);
+  const handleChangeFirstName = ({ target }) =>
+    setCustomer((customer) => ({
+      ...customer,
+      firstName: target.value,
+    }));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(customer);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        value={customer.firstName}
+        onChange={handleChangeFirstName}
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+};
